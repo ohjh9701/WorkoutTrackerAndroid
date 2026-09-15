dc765a70d4e6b449d77c01fcc75d73fc27392e4e
OH Jihun <ohjh4875@gmail.com>
2026-09-15 17:27:37 +0900

    Change Compose BOM version in build.gradle.kts
    Updated Compose BOM version to 2026.04.01.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+    val composeBom = platform("androidx.compose:compose-bom:2026.04.01")
     implementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
